ee7c7552e910dfd9afde54382a5f5ee7eca8c022
Anthony Johnson <anthonythomasjohnson@outlook.com>
2021-09-10 15:57:34 +0900

    added restaurant show page

--- app/controllers/api/v1/restaurants_controller.rb
+++ app/controllers/api/v1/restaurants_controller.rb
@@ -1,5 +1,19 @@
 class Api::V1::RestaurantsController < Api::V1::BaseController
+  # before_action :set_restaurant, only: [ :show ]
+  
   def index
     @restaurants = policy_scope(Restaurant)
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+    authorize @restaurant  # For Pundit  
+  end
+
+  private
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:id])
+    authorize @restaurant  # For Pundit
   end
 end
